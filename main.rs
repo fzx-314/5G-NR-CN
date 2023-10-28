3062189248f340d0faf9fdd485838b349cd09e84
keval <maldekeval@ymail.com>
2023-10-28 22:38:09 +0530

    Minor edit

---
+++ main.rs
@@ -0,0 +1,9 @@
+// Sample Hello world in rust
+
+
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+
+    // Print text to the console.
+    println!("Hello World!");
+}
